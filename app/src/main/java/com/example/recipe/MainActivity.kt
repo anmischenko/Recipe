543918f97f89683fd6159c8a521697d6b642b693
Angelina <an.mischenko2002@gmail.com>
2023-09-17 12:55:20 +0300

    Add ListItem

--- app/src/main/java/com/example/recipe/MainActivity.kt
+++ app/src/main/java/com/example/recipe/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.recipe
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import com.example.recipe.ui.theme.RecipeTheme
 import com.example.recipe.ui_components.DrawerMenu
+import com.example.recipe.ui_components.MainListItem
 import com.example.recipe.ui_components.MainTopBar
 import com.example.recipe.utils.DrawerEvents
+import com.example.recipe.utils.IdArrayList
+import com.example.recipe.utils.ListItem
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -19,8 +27,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             val scaffoldState = rememberScaffoldState()
             val coroutineScope = rememberCoroutineScope()
+            val mainList = remember {
+                mutableStateOf(getListItemsByIndex(0, this))
+            }
             val topBarTitle = remember {
-                mutableStateOf("Сырники")
+                mutableStateOf("Выпечка")
             }
             RecipeTheme {
                 Scaffold(
@@ -36,6 +47,7 @@ class MainActivity : ComponentActivity() {
                             when(event) {
                                 is DrawerEvents.OnItemClick -> {
                                     topBarTitle.value = event.title
+                                    mainList.value = getListItemsByIndex(event.index, this@MainActivity)
                                 }
                             }
                             coroutineScope.launch {
@@ -45,9 +57,28 @@ class MainActivity : ComponentActivity() {
                         }
                     }
                 ) {
-
+                    LazyColumn(modifier = Modifier.fillMaxSize()) {
+                        items(mainList.value) { item ->
+                            MainListItem(item = item)
+                        }
+                    }
                 }
             }
         }
     }
+}
+
+private fun getListItemsByIndex(index: Int, context: Context): List<ListItem> {
+    val list = ArrayList<ListItem>()
+    val arrayList = context.resources.getStringArray(IdArrayList.listId[index])
+    arrayList.forEach { item ->
+        val itemArray = item.split("|")
+        list.add(
+            ListItem(
+                itemArray[0],
+                itemArray[1]
+            )
+        )
+    }
+    return list
 }
---
+++ app/src/main/java/com/example/recipe/utils/IdArrayList.kt
@@ -0,0 +1,12 @@
+package com.example.recipe.utils
+
+import com.example.recipe.R
+
+object IdArrayList {
+    val listId = listOf(
+        R.array.bakery,
+        R.array.ice_cream,
+        R.array.salat,
+        R.array.sup
+    )
+}
